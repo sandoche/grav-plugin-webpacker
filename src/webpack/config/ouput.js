892b740537f47e9b57c3ce448a6c86f222a8c5af
jimblue <mr.jimblue@gmail.com>
2018-06-05 20:30:18 +0200

    improve build hash based on each chunk for a better long term caching

--- src/webpack/config/ouput.js
+++ src/webpack/config/ouput.js
@@ -6,7 +6,7 @@
 module.exports = () => {
   const output = {
     path: GravConfig.outputPath,
-    filename: GravConfig.dev ? 'js/[name].js' : 'js/[name].[hash].js',
+    filename: GravConfig.dev ? 'js/[name].js' : 'js/[name].[chunkhash].js',
     publicPath: GravConfig.dev ? '/assets/' : GravConfig.publicPath
   }
 
